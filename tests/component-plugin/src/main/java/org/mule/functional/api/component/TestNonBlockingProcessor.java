c9347a099b44244ddf2e2e3dce4eb0f188515697
Juli <luzzijulieta@gmail.com>
2020-01-09 17:02:56 -0300

    MULE-16528: remove  in foreach

--- tests/component-plugin/src/main/java/org/mule/functional/api/component/TestNonBlockingProcessor.java
+++ tests/component-plugin/src/main/java/org/mule/functional/api/component/TestNonBlockingProcessor.java
@@ -75,6 +75,6 @@ public class TestNonBlockingProcessor extends AbstractComponent
 
   @Override
   public void dispose() {
-    customScheduler.ifComputed(sch -> sch.stop());
+    customScheduler.ifComputed(Scheduler::stop);
   }
 }
